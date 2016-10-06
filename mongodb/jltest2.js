b21509c1abc1ec967038bdae072a3827565b07a0
jamesl <james.lapointe@gmail.com>
2016-10-05 22:33:10 -0700

    mongo add

---
+++ mongodb/jltest2.js
@@ -0,0 +1,33 @@
+db.qrresource.insert({
+  "resource": {
+    "id": "4e30a30a-859d-11e6-a347-2fdc052a443f",
+    "ownerid": "c0949584-8a78-11e6-83bb-f3faf917636c",
+    "description": "test yahoo resource",
+    "resources": [{
+      "action": "forward",
+      "address": "https://yahoo.com"
+    }]
+  }
+} );
+db.qrresource.insert( {
+  "resource": {
+    "id": "606c12fc-8a7a-11e6-8c88-3b029441eb6e",
+    "ownerid": "60fb6dbc-8a7a-11e6-afdf-2f4a5b76768d",
+    "description": "linked in",
+    "resources": [{
+      "action": "forward",
+      "address": "https://www.linkedin.com"
+    }]
+  }
+} );
+db.qrresource.insert({
+  "resource": {
+    "id": "3eb8a3da-8a7c-11e6-89b3-8f4093f5abd6",
+    "ownerid": "400c9a70-8a7c-11e6-97cc-f31d8434e7f9",
+    "description": "google resource",
+    "resources": [{
+      "action": "forward",
+      "address": "https://www.google.com"
+    }]
+  }
+});
