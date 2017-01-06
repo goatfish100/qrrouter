fa3cda775e5cb6ea2f3c4cc0fef42d089dee09fc
james lapointe <james.lapointe@gmail.com>
2017-01-06 14:52:43 -0700

    adding mongo - startup/int tests

--- qrrouter/mongo_test.go
+++ qrrouter/mongo_test.go
@@ -1,15 +1,36 @@
 package main
 
-import "testing"
-import "gopkg.in/mgo.v2/dbtest"
+import (
+	"log"
+	"os"
+	"testing"
+
+	"gopkg.in/mgo.v2/dbtest"
+)
+
 import "io/ioutil"
-import "os"
 
 var ()
 
+// var resource1 = Resource{
+// 	"Id": "584f89b595f3b0c9cab0d38a", "Description": "test", "Protected": "false", "Action": "forward", "Address": "www.yahoo.com"}
+var resource1 = Resource{Uuid: "034c1dd2-d454-11e6-a110-b34e9f2c654a", Description: "yahoo", Protected: "false", Action: "forward", Address: "http://www.yahoo.com"}
+var resource2 = Resource{Uuid: "059edd7c-d454-11e6-92b9-374c2fc3d623", Description: "yahoo", Protected: "false", Action: "forward", Address: "http://www.yahoo.com"}
+
+//{"_id" : ObjectId("584f89b595f3b0c9cab0d38a"), "description" : "test", "protected" : "false", "action" : "redirect", "address" : "https://www.yahoo.com" }
 // Server holds the dbtest DBServer
 var Server dbtest.DBServer
 
+func insertFixtures() {
+
+	if err := session.DB(MongoDBDatabase).C("res").Insert(resource1); err != nil {
+		log.Println(err)
+	}
+
+	if err := session.DB(MongoDBDatabase).C("res").Insert(resource2); err != nil {
+		log.Println(err)
+	}
+}
 func init() {
 
 }
@@ -21,10 +42,12 @@ func TestMain(m *testing.M) {
 
 	// My main session var is now set to the temporary MongoDB instance
 	session = Server.Session()
+	insertFixtures()
 	// Run the test suite
 	retCode := m.Run()
 	// Make sure we DropDatabase so we make absolutely sure nothing is left or locked while wiping the data and
 	// close session
+
 	session.DB(MongoDBDatabase).DropDatabase()
 	session.Close()
 
--- qrrouter/resource.go
+++ qrrouter/resource.go
@@ -2,6 +2,7 @@ package main
 
 type Resource struct {
 	Id          string `json:"id" bson:"_id,omitempty"`
+	Uuid        string `json:"uuid"`
 	Description string `json:"Description"`
 	Protected   string `json:"Protected"`
 	Action      string `json:"Action"`
