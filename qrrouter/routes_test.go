0ff717c4b5eb242bbb6eb37c8a33f7c3116c4c51
james lapointe <james.lapointe@gmail.com>
2017-02-01 16:16:59 -0600

    fix bug

--- qrrouter/routes_test.go
+++ qrrouter/routes_test.go
@@ -34,3 +34,23 @@ func TestGorilla(t *testing.T) {
 	fmt.Println(string(rr.Body.Bytes()))
 
 }
+
+func TestUUIDRoute(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/uuid/059edd7c-d454-11e6-92b9-374c2fc3d623", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(UUIDHandler)
+
+	handler.ServeHTTP(rr, req)
+	//
+	if rr.Code == 404 {
+		fmt.Println("asdf")
+	}
+	fmt.Println("testUUIDRoute code ", rr.Code)
+	//fmt.Println(rr.Result())
+	fmt.Println(string(rr.Body.Bytes()))
+
+}
