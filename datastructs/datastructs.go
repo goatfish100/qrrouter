3bd4458b846c61e569b570bbeb9ebafd45cce2a5
james lapointe <james.lapointe@gmail.com>
2017-02-02 15:18:15 -0600

    added jsonsuccess to file

--- datastructs/datastructs.go
+++ datastructs/datastructs.go
@@ -4,7 +4,7 @@ import "gopkg.in/mgo.v2/bson"
 
 type Org struct {
 	//Id         string `json:"id" bson:"_id,omitempty"`
-	Id         bson.ObjectId `json:"id"        bson:"_id,omitempty"`
+	ID         bson.ObjectId `json:"id"        bson:"_id,omitempty"`
 	Orgname    string        `json:"orgname"`
 	Address    string        `json:"address"`
 	City       string        `json:"city"`
@@ -24,11 +24,15 @@ type Orgs []Org
 
 type Users []User
 type Resource struct {
-	Id          string `json:"id" bson:"_id,omitempty"`
+	ID          string `json:"id" bson:"_id,omitempty"`
 	Uuid        string `json:"uuid"`
-	OrgId          string `json:"orgid" bson:"orgid,omitempty"`
+	OrgId       string `json:"orgid" bson:"orgid,omitempty"`
 	Description string `json:"Description"`
 	Protected   string `json:"Protected"`
 	Action      string `json:"Action"`
 	Address     string `json:"Address"`
+}
+
+type JSONSuccess struct {
+	Success string `json:"success"`
 }
