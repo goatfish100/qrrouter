3e41761db607de2e212ac5152631e933a0640dfd
james lapointe <james.lapointe@gmail.com>
2017-02-19 22:54:55 -0500

    test file for docker - related

--- web_page.go
+++ web_page.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/gorilla/sessions"
 	"gopkg.in/mgo.v2"
+	"os"
 )
 
 var store = sessions.NewCookieStore([]byte("something-very-secret"))
-var mgosession, err = mgo.Dial("localhost")
+var mongohost = os.Getenv("MONGO_HOST")
+var mgosession, err = mgo.Dial(mongohost)
 
 //MongoDBDatabase - mongo database name
 var MongoDBDatabase = "resources"
