9045f8f16ee948dceebe90bd060027554ba22623
james lapointe <james.lapointe@gmail.com>
2017-03-03 10:13:22 -0500

    refactoring

---
+++ mongocall_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+
+	datastructs "bitbucket.org/goatfish100/qrhelperdatastructs"
+)
+
+func TestFetchResource(t *testing.T) {
+	t.Log("TestFetchResource Test")
+
+	testResult := FetchResource("444edd7c-d454-11e6-92b9-374c2fc3d623")
+	if testResource(testResult, resource2) == false {
+		t.Log("feched resource is not same as inserted resource")
+		t.Fail()
+	}
+
+}
+
+func testResource(r1 datastructs.Resource, r2 datastructs.Resource) bool {
+	var resflag bool
+	if r1.Action == r2.Action &&
+		r1.Address == r2.Address &&
+		r1.Description == r2.Description &&
+		r1.Protected == r2.Protected &&
+		r1.Uuid == r2.Uuid {
+		resflag = true
+	} else {
+		resflag = false
+	}
+
+	return resflag
+
+}
