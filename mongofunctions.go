0639f5268491450766d2d68ee9f5d2ab27c5f910
james lapointe <james.lapointe@gmail.com>
2017-08-12 10:39:13 -0700

    fix for broken code - when trying to put upsert counter

--- mongofunctions.go
+++ mongofunctions.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 
-	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
 	"gitlab.com/qrhelper/qrhelperdatastructs"
@@ -12,14 +11,15 @@ import (
 //FetchResource - fetch resource
 func FetchResource(resourceid string) datastructs.Resource {
 
-	change := mgo.Change{
-		Update:    bson.M{"$inc": bson.M{"n": 1}},
-		ReturnNew: true,
-	}
+	// change := mgo.Change{
+	// 	Update:    bson.M{"$inc": bson.M{"n": 1}},
+	// 	ReturnNew: true,
+	// }
 
 	c := MgoSession.DB(MongoDBDatabase).C("res")
 	result := datastructs.Resource{}
-	info err = c.Find(bson.M{"uuid": resourceid}).One(&result).Change(result)
+
+	err = c.Find(bson.M{"uuid": resourceid}).One(&result)
 	//ls err := result.Apply(change, &result)
 
 	if err != nil {
